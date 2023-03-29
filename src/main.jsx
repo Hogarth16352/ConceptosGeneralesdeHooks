6e7d6000fbb5b747faad51a235a520a639c052bf
Hogarth16352 <hogarth16352@gmail.com>
2023-03-28 19:36:17 -0600

    Uso de useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Padre } from './07-tarea-memo/Padre'
 // import { HooksApp } from './HooksApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -12,10 +11,13 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallBackHook } from './06-memos/CallbackHook'
+// import { Padre } from './07-tarea-memo/Padre'
+import { TodoApp } from './08-useReducer/TodoApp'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />
+    <TodoApp />
   // </React.StrictMode>,
 )
